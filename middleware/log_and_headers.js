6e9600eb377a727cf5740d0e4f6cff4b6b57b397
Sajana <mentorbridge.lk@gmail.com>
2025-10-18 23:37:41 +0530

    chore(debug): add CORS request/response logger

---
+++ middleware/log_and_headers.js
@@ -0,0 +1,12 @@
+﻿module.exports = function(app){
+  app.use((req,res,next)=>{
+    console.log("[CORS-DEBUG] Request:", req.method, req.path);
+    // after response finishes, log the headers that were sent
+    const _end = res.end;
+    res.end = function(...args){
+      console.log("[CORS-DEBUG] Response headers:", JSON.stringify(res.getHeaders()));
+      return _end.apply(this, args);
+    };
+    next();
+  });
+};
